5f942f8c750afd4cdc22a6be02757e5d565163ca
Thomas Gillam <tpgillam@googlemail.com>
2021-02-11 22:11:27 +0000

    Declare that we should work on python 3.9

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Stransverse mass computation as a numpy ufunc.",
     install_requires=["numpy"],
