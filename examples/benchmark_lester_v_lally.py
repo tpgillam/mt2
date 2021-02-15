32a10d3a15cb8ffbaeec85ca24479245bc7c8290
Thomas Gillam <tpgillam@googlemail.com>
2021-02-15 21:04:21 +0000

    Fix benchmark to actually compare the two implementations!

--- examples/benchmark_lester_v_lally.py
+++ examples/benchmark_lester_v_lally.py
@@ -14,28 +14,30 @@ def main():
     mass_2 = numpy.linspace(1, 200, n2).reshape((1, -1))
 
     # Pre-allocate output so that we are just timing MT2 itself, not the allocation of
+    
+    args = (
+        100, 410, 20,  # Visible 1: mass, px, py
+        150, -210, -300,  # Visible 2: mass, px, py
+        -200, 280,  # Missing transverse momentum: x, y
+        mass_1, mass_2,  # Invisible 1 mass, invisible 2 mass
+    )
+
     # the output buffer.
-    out = numpy.zeros((n1, n2))
+    out_lester = numpy.zeros((n1, n2))
+    out_lally = numpy.zeros((n1, n2))
 
     # `val` has shape (n1, n2), since `mass_1` and `mass_2` broadcast.
     t_lester_start = time.time()
-    mt2(
-        100, 410, 20,  # Visible 1: mass, px, py
-        150, -210, -300,  # Visible 2: mass, px, py
-        -200, 280,  # Missing transverse momentum: x, y
-        mass_1, mass_2,  # Invisible 1 mass, invisible 2 mass
-        out=out)
+    mt2(*args, out=out_lester)
     t_lester_end = time.time()
     
     t_lally_start = time.time()
-    mt2(
-        100, 410, 20,  # Visible 1: mass, px, py
-        150, -210, -300,  # Visible 2: mass, px, py
-        -200, 280,  # Missing transverse momentum: x, y
-        mass_1, mass_2,  # Invisible 1 mass, invisible 2 mass
-        out=out)
+    mt2_lally(*args, out=out_lally)
     t_lally_end = time.time()
     
+    # Check that we get the same thing.
+    numpy.testing.assert_array_almost_equal(out_lester, out_lally)
+
     print("Elapsed time Lester: {} seconds".format(t_lester_end - t_lester_start))
     print("Elapsed time Lally : {} seconds".format(t_lally_end - t_lally_start))
 
