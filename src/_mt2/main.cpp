28efa6ecaf4fa6caae9a9a9ca4f11b4ea88d57f7
Lindsey Gray <lindsey.gray@gmail.com>
2025-02-12 15:38:16 -0600

    mark mt2 as freethreaded/threadsafe (#74)
    * chore: mark mt2 as freethreaded/threadsafe
    
    * add testing in 3.13t, wheels are handled by 3.13 alone
    
    * single phase initialization
    
    * add freethreaded wheel builds
    
    * update cibuildwheel
    
    * auto32 build no longer works for py313(t)
    
    * try more uniform labelling
    
    Co-authored-by: Tom Gillam <tpgillam@googlemail.com>
    
    ---------
    
    Co-authored-by: Tom Gillam <tpgillam@googlemail.com>

--- src/_mt2/main.cpp
+++ src/_mt2/main.cpp
@@ -312,6 +312,10 @@ PyMODINIT_FUNC PyInit__mt2(void)
         return NULL;
     }
 
+#ifdef Py_GIL_DISABLED
+    PyUnstable_Module_SetGIL(module, Py_MOD_GIL_NOT_USED);
+#endif
+    
     import_array();
     import_ufunc();
     import_umath();
